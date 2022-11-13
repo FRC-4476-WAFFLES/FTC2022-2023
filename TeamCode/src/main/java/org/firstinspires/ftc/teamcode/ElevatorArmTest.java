e4a52a506dfc6508f14064fa875df790b2a58e77
wafflespc <116522294+wafflespc@users.noreply.github.com>
2022-11-13 17:32:57 -0500

    Create ElevatorArmTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ElevatorArmTest.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Subsystems.ElevatorArmSubsystem;
+
+@TeleOp(name = "Elevator Arm Test")
+public class ElevatorArmTest extends LinearOpMode {
+    private MotorEx leftElevatorMotor;
+    private MotorEx rightElevatorMotor;
+    private MotorEx armMotor;
+
+    private GamepadEx controller;
+
+    private ElevatorArmSubsystem elevatorArm;
+
+    @Override
+    public void runOpMode() {
+        leftElevatorMotor = new MotorEx(hardwareMap, "leftElevatorMotor");
+        rightElevatorMotor = new MotorEx(hardwareMap, "rightElevatorMotor");
+        armMotor = new MotorEx(hardwareMap, "armMotor");
+
+        controller = new GamepadEx(gamepad1);
+
+        elevatorArm = new ElevatorArmSubsystem(
+                leftElevatorMotor,
+                rightElevatorMotor,
+                armMotor
+        );
+
+        elevatorArm.initialize();
+
+        waitForStart();
+        while (opModeIsActive()) {
+            if (controller.getButton(GamepadKeys.Button.A)) {
+                elevatorArm.setTargetLevel(ElevatorArmSubsystem.Levels.GROUNDED);
+            } else if (controller.getButton(GamepadKeys.Button.B)) {
+                elevatorArm.setTargetLevel(ElevatorArmSubsystem.Levels.L1);
+            } else if (controller.getButton(GamepadKeys.Button.X)) {
+                elevatorArm.setTargetLevel(ElevatorArmSubsystem.Levels.L2);
+            } else if (controller.getButton(GamepadKeys.Button.Y)) {
+                elevatorArm.setTargetLevel(ElevatorArmSubsystem.Levels.L3);
+            }
+
+            
+        }
+    }
+}
