6ed0f8c5e927e95f7f20df01448c89341818142c
wafflespc <116522294+wafflespc@users.noreply.github.com>
2022-11-15 19:45:57 -0500

    Camera Stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/CameraSubsystem.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+public class CameraSubsystem extends SubsystemBase {
+    OpenCvCamera webcam;
+    int cameraMonitorViewId;
+
+    SignalSleevePipeline pipeline;
+
+    public CameraSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
+        // Get webcam object from hardwareMap
+        this.cameraMonitorViewId = hardwareMap.appContext
+                .getResources().getIdentifier(
+                        "cameraMonitorViewId",
+                        "id",
+                        hardwareMap.appContext.getPackageName()
+                );
+        this.webcam = OpenCvCameraFactory.getInstance()
+                .createWebcam(hardwareMap.get(
+                        WebcamName.class, "Webcam 1"
+                ), this.cameraMonitorViewId);
+
+        // Set pipeline to Freight Detector
+        this.pipeline = new SignalSleevePipeline(telemetry);
+        this.webcam.setPipeline(pipeline);
+
+        // Activate camera
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                // Start streaming from the camera
+                webcam.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+                telemetry.addLine("Opened webcam.");
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                // Camera fails to be opened
+                telemetry.addData("Failed to open webcam. Error code:", errorCode);
+                telemetry.update();
+            }
+        });
+    }
+
+    public int getParkingLocation() {
+        return this.pipeline.getParkingLocation();
+    }
+
+    public void stop() {
+        this.webcam.stopStreaming();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/SignalSleevePipeline.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class SignalSleevePipeline extends OpenCvPipeline {
+    Telemetry telemetry;
+
+    Mat image = new Mat();
+
+    private int parkingLocation;
+
+    public Rect SignalSleeveROI = new Rect(
+            new Point(100, 0),
+            new Point(300, 300)
+    );
+
+    public SignalSleevePipeline(Telemetry telemetry) {
+        this.telemetry = telemetry;
+        this.parkingLocation = 0;
+    }
+
+    @Override
+    public Mat processFrame(Mat frame) {
+
+
+        return frame;
+    }
+
+    public int getParkingLocation() {
+        return this.parkingLocation;
+    }
+}
