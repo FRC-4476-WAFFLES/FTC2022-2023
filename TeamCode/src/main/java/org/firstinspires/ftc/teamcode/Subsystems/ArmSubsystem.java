4c8d4d07d37fb6c4040f73350b35b604af65f4ec
Benjamin4476 <bendenotterversteeg@gmail.com>
2022-12-05 21:37:34 -0500

    Added PIDF position control to arm motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ArmSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ArmSubsystem.java
@@ -2,39 +2,40 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import lib.autoNavigation.math.MathUtil;
+import lib.hardware.motors.MotorExPositionPIDF;
 
 public class ArmSubsystem extends SubsystemBase {
-    private final Telemetry telemetry;
-
-    private final MotorEx armMotor;
-
-    private final double TICKS_PER_ROTATION_ARM = Motor.GoBILDA.RPM_435.getCPR(); // TODO: Change this to reflect what gearbox is on the arm motor
-
-    private final double ARM_MAX_TICKS_PER_SECOND = 20;
-
-    private int targetPosition;
-
     private final ElapsedTime elapsedTime = new ElapsedTime();
     private double previousTime = 0;
     private double previousLoopTime = 0;
 
+    private final Telemetry telemetry;
+
+    private final MotorExPositionPIDF armMotor;
+
+    private final double TICKS_PER_DEGREE = Motor.GoBILDA.RPM_435.getCPR() * 5.0 / 360.0;
+    private final int POS_HORIZONTAL = 200; // TODO: Measure motor position when arm is horizontal
+    private final double maxGravityFF = 0.07; // TODO: Set this number to be the maximum motor power applied by feedforward
+
+    private int targetPosition;
+
     public ArmSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
         this.telemetry = telemetry;
 
-        armMotor = new MotorEx(hardwareMap, "Arm", Motor.GoBILDA.RPM_435);
+        armMotor = new MotorExPositionPIDF(hardwareMap, "Arm", Motor.GoBILDA.RPM_435);
 
         armMotor.setRunMode(Motor.RunMode.PositionControl);
         armMotor.resetEncoder();
         armMotor.setInverted(true);
         armMotor.setPositionTolerance(15);
-        armMotor.setPositionCoefficient(0.01);
+        armMotor.setPID(0.0, 0.0, 0.0); // TODO: Test and set values for these coefficients, starting with arbitrary feedforward
+        armMotor.setF(0.0);
         armMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
 
         targetPosition = 0;
@@ -42,15 +43,23 @@ public class ArmSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
-        this.previousLoopTime = this.elapsedTime.seconds() - this.previousTime;
-        this.previousTime = this.elapsedTime.seconds();
+        previousLoopTime = this.elapsedTime.seconds() - this.previousTime;
+        previousTime = this.elapsedTime.seconds();
 
-        this.armMotor.setTargetPosition(this.targetPosition);
+        int currentPos = armMotor.getCurrentPosition();
+        double degrees = (currentPos - POS_HORIZONTAL) / TICKS_PER_DEGREE;
+        double radians = Math.toRadians(degrees);
+        double cosineScalar = Math.cos(radians);
 
-        this.armMotor.set(1.0);
+        armMotor.setF(maxGravityFF * cosineScalar);
+
+        armMotor.setTargetPosition(this.targetPosition);
+
+        armMotor.set(1.0);
 
         telemetry.addData("arm pos", armMotor.getCurrentPosition());
         telemetry.addData("target pos", targetPosition);
+        telemetry.addData("applied feedforward value", maxGravityFF * cosineScalar);
     }
 
     public void setTargetPosition(Positions position) {
@@ -62,7 +71,7 @@ public class ArmSubsystem extends SubsystemBase {
     }
 
     public void moveTargetPositionWithJoystick(double value) {
-        moveTargetPosition((int) (value * previousLoopTime * 2.0)); // TODO: adjust magic number to make thing work good
+        moveTargetPosition((int) (value * previousLoopTime * 20.0)); // TODO: adjust magic number to make thing work good
     }
 
     public void stop() {
